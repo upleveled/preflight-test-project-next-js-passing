c654688397e300d395d1d3a12760ebb62e4f072d
Karl Horky <karl.horky@gmail.com>
2023-10-14 11:38:01 +0200

    Remove unnecessary steps and steps requiring root

--- scripts/postgresql-setup-and-start.sh
+++ scripts/postgresql-setup-and-start.sh
@@ -4,19 +4,18 @@
 set -o errexit
 
 echo "Setting up PostgreSQL..."
-mkdir /run/postgresql
-chown postgres:postgres /run/postgresql
+# mkdir /run/postgresql
+# chown postgres:postgres /run/postgresql
 mkdir /var/lib/postgresql/data
 # Only allow postgres user access to data directory
 chmod 0700 /var/lib/postgresql/data
 initdb -D /var/lib/postgresql/data
-chown -R postgres:postgres /var/lib/postgresql/data
 # Log to syslog, which is rotated (older logs are
 # automatically deleted)
 sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i /var/lib/postgresql/data/postgresql.conf
 
 echo "Starting PostgreSQL..."
-su - postgres -c "pg_ctl start -D /var/lib/postgresql/data"
+pg_ctl start -D /var/lib/postgresql/data
 
 echo "Creating database, user and schema..."
 psql -U postgres postgres << SQL
