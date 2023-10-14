11eef93acd0d4c63fe2eac0fb595fd6c7e47ca22
Karl Horky <karl.horky@gmail.com>
2023-10-14 11:14:24 +0200

    Start cleaning up script

--- scripts/postgresql-setup-and-start.sh
+++ scripts/postgresql-setup-and-start.sh
@@ -5,24 +5,14 @@ set -o errexit
 
 echo "Setting up PostgreSQL..."
 mkdir /run/postgresql
-echo ">>>>>>>>>>>>>>>>>>>>>>>>2"
 chown postgres:postgres /run/postgresql
-echo ">>>>>>>>>>>>>>>>>>>>>>>>3"
-# su - postgres
-# Home directory for postgres user
-echo ">>>>>>>>>>>>>>>>>>>>>>>>4"
-# cd /var/lib/postgresql
-echo ">>>>>>>>>>>>>>>>>>>>>>>>5"
 mkdir /var/lib/postgresql/data
 # Only allow postgres user access to data directory
-echo ">>>>>>>>>>>>>>>>>>>>>>>>6"
 chmod 0700 /var/lib/postgresql/data
 chown postgres:postgres /var/lib/postgresql/data
-echo ">>>>>>>>>>>>>>>>>>>>>>>>7"
 su - postgres -c "initdb -D /var/lib/postgresql/data"
 # Log to syslog, which is rotated (older logs are
 # automatically deleted)
-echo ">>>>>>>>>>>>>>>>>>>>>>>>8"
 sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i /var/lib/postgresql/data/postgresql.conf
 
 echo "Starting PostgreSQL..."
