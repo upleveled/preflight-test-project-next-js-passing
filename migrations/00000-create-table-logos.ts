ce07b9fe9ada70337501092c0af6924b4234aa0e
Karl Horky <karl.horky@gmail.com>
2024-02-05 23:08:46 +0100

    Upgrade packages, remove more .mjs extensions

--- migrations/00000-create-table-logos.ts
+++ migrations/00000-create-table-logos.ts
@@ -3,30 +3,22 @@ import { Sql } from 'postgres';
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE logos (
-      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-      NAME VARCHAR(20) NOT NULL
+      id integer PRIMARY key generated always AS identity,
+      name varchar(20) NOT NULL
     )
   `;
 
   await sql`
     INSERT INTO
-      logos (NAME)
+      logos (name)
     VALUES
-      (
-        'Next.js'
-      ),
-      (
-        'Next.js'
-      ),
-      (
-        'Next.js'
-      ),
-      (
-        'Next.js'
-      )
+      ('Next.js'),
+      ('Next.js'),
+      ('Next.js'),
+      ('Next.js')
   `;
 }
 
 export async function down(sql: Sql) {
-  await sql` DROP TABLE logos `;
+  await sql`DROP TABLE logos`;
 }
