84c2b1ff90c5d5a2a27e8b8131bfcb7e03192649
Karl Horky <karl.horky@gmail.com>
2024-04-01 13:33:22 +0200

    Add simple script with connection string

---
+++ db.js
@@ -0,0 +1,20 @@
+import postgres from 'postgres';
+
+// import { setEnvironmentVariables } from './util/config';
+
+// setEnvironmentVariables();
+
+const sql = postgres(
+  'postgres://preflight_test_project_next_js_passing:preflight_test_project_next_js_passing@localhost:5432/preflight_test_project_next_js_passing',
+);
+
+console.log(
+  await sql`
+    SELECT
+      1
+  `,
+);
+
+// This is only for the example, in your code you will want
+// a persistent connection to the database
+await sql.end();
