2e75908fa795950a7930093ec0ecdb9f2a5cb8a1
Karl Horky <karl.horky@gmail.com>
2024-04-01 17:22:07 +0200

    Update ley.config.js

--- ley.config.js
+++ ley.config.js
@@ -1,7 +1,7 @@
 import { setEnvironmentVariables } from './util/config.js';
 
 setEnvironmentVariables();
-
+console.log(process.env)
 const options = {
   ssl: Boolean(process.env.POSTGRES_URL),
 };
