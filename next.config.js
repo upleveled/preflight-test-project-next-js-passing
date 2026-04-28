d95109af80b5005042a908c6fefcebee36a23085
Karl Horky <karl.horky@gmail.com>
2026-04-28 18:49:32 +0200

    Configure Next.js build to skip TS checks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typedRoutes: true,
+    typescript: {
+    // Ignore TypeScript errors because types already checked during CI
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
